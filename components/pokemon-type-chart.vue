2c1a6c7169976953483e9d1b19e6e0ada3233a81
Bart Haasbroek <bart.haasbroek@rabobank.nl>
2022-01-12 09:12:56 +0100

    change labels in poke-api

--- components/pokemon-type-chart.vue
+++ components/pokemon-type-chart.vue
@@ -22,30 +22,13 @@
       <div class="mt-3 type-chart">
         <div>
           <div class="type-chart__category">
-            2x effectief tegen {{ typeToCheck }}
+            Super effective tegen {{ typeToCheck }}
+            <p>(type -> {{ typeToCheck }} = super effective)</p>
           </div>
 
           <b-list-group>
             <b-list-group-item
-              v-for="(type, index) in data.vulnerableTo"
-              :key="index + type"
-            >
-              {{ type }}
-            </b-list-group-item>
-          </b-list-group>
-        </div>
-        <div v-if="data['doubleVulnerability']">
-          <div class="type-chart__category">
-            4x effectief tegen {{ typeToCheck }}
-          </div>
-
-          <div v-if="data.doubleVulnerability.length === 0">
-            Er zijn geen types 4x effectief
-          </div>
-
-          <b-list-group v-else>
-            <b-list-group-item
-              v-for="(type, index) in data.doubleVulnerability"
+              v-for="(type, index) in data.superEffective"
               :key="index + type"
             >
               {{ type }}
@@ -55,6 +38,7 @@
         <div>
           <div class="type-chart__category">
             Niet effectief tegen {{ typeToCheck }}
+            <p>(type -> {{ typeToCheck }} = Not very effective)</p>
           </div>
 
           <b-list-group>
@@ -68,7 +52,8 @@
         </div>
         <div>
           <div class="type-chart__category">
-            {{ typeToCheck }} is 2 effectief tegen:
+            {{ typeToCheck }} is super effectief tegen:
+            <p>{{ typeToCheck }} --> type = super effective)</p>
           </div>
 
           <b-list-group>
@@ -83,6 +68,7 @@
         <div>
           <div class="type-chart__category">
             {{ typeToCheck }} is niet effectief tegen:
+            <p>{{ typeToCheck }} --> type = not very effective)</p>
           </div>
 
           <b-list-group>
@@ -192,7 +178,17 @@ export default {
       let { data } = await this.$axios.get(
         `/api/pokemon-types?type=${typeToCheck}`
       );
-      this.data = data;
+
+      const doubleVulnerability = data["doubleVulnerability"] || [];
+
+      this.data = {
+        ...data,
+        superEffective: [
+          ...doubleVulnerability.map(t => `${t} (4x Effective)`),
+          ...data.vulnerableTo.map(t => `${t} (2x Effective)`)
+        ]
+      };
+      console.log("data", this.data);
       this.typeToCheck = typeToCheck;
     }
   },
@@ -215,7 +211,7 @@ export default {
 }
 .type-chart {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-gap: 16px;
 
   &__category {
